9be77279c655867abe1bc3de276f88098d24422f
Phill <philldamaskin@gmail.com>
2025-04-26 12:26:50 -0400

    use update function when waiting for compliance

--- tests/ImportTests.cs
+++ tests/ImportTests.cs
@@ -20,7 +20,7 @@ namespace Materials.Systems.Tests
 
             Material material = new(world, "Assets/testMaterial.json");
 
-            await material.UntilCompliant(Simulate, cancellation);
+            await material.UntilCompliant(Update, cancellation);
 
             Assert.That(material.IsCompliant, Is.True);
             Shader vertexShader = material.VertexShader;
